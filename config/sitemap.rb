0c3c095fcc650b9713da5c494dab582f1e3ed6ae
George Morris <gmorris1242@gmail.com>
2019-12-11 14:35:11 -0500

    Add working sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,27 +1,28 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "http://www.badmagnets.com"
+SitemapGenerator::Sitemap.compress = false
 
 SitemapGenerator::Sitemap.create do
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically for you.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: add(path, options={})
-  #        (default options are used if you don't specify)
-  #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly',
-  #           :lastmod => Time.now, :host => default_host
-  #
-  # Examples:
-  #
-  # Add '/articles'
-  #
-  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
-  #
-  # Add all articles:
-  #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
+  routes = Rails.application.routes.routes.map do |route|
+    {
+      alias: route.name,
+      path: route.path.spec.to_s,
+      controller: route.defaults[:controller],
+      action: route.defaults[:action]
+    }
+  end
+
+  routes.reject! { |route| route[:path] == '/' }
+
+  routes.uniq! { |route| route[:path] }
+
+  skip_strings  = ['rails','email_list']
+
+  routes.reject! do |route|
+    skip_strings.any? { |skip_string| route[:path].include?(skip_string) }
+  end
+
+  routes.each do |route|
+    add route[:path][0..-11]
+  end
 end
